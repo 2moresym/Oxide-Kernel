37cd5d0802a6581198f6c42ee4109c3d941875f9
2vex <2nosym@gmail.com>
2026-09-02 01:00:48 -0500

    Add diagnostics and memory foundation: src/cpu/gdt.rs

---
+++ src/cpu/gdt.rs
@@ -0,0 +1,45 @@
+use spin::Once;
+use x86_64::{
+    instructions::segmentation::{Segment, CS},
+    instructions::tables::load_tss,
+    structures::{
+        gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector},
+        tss::TaskStateSegment,
+    },
+    VirtAddr,
+};
+
+pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+const DOUBLE_FAULT_STACK_SIZE: usize = 4096 * 5;
+
+static GDT: Once<(GlobalDescriptorTable, Selectors)> = Once::new();
+static TSS: Once<TaskStateSegment> = Once::new();
+static mut DOUBLE_FAULT_STACK: [u8; DOUBLE_FAULT_STACK_SIZE] = [0; DOUBLE_FAULT_STACK_SIZE];
+
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
+}
+
+pub fn init() {
+    let tss = TSS.call_once(|| {
+        let mut tss = TaskStateSegment::new();
+        let stack_start = VirtAddr::from_ptr(&raw const DOUBLE_FAULT_STACK);
+        let stack_end = stack_start + DOUBLE_FAULT_STACK_SIZE;
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = stack_end;
+        tss
+    });
+
+    let (gdt, selectors) = GDT.call_once(|| {
+        let mut gdt = GlobalDescriptorTable::new();
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(tss));
+        (gdt, Selectors { code_selector, tss_selector })
+    });
+
+    gdt.load();
+    unsafe {
+        CS::set_reg(selectors.code_selector);
+        load_tss(selectors.tss_selector);
+    }
+}
