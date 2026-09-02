399b7c5037036542c31fcb167749b90f3266a31a
2vex <2nosym@gmail.com>
2026-09-02 01:00:46 -0500

    Add diagnostics and memory foundation: src/cpu/interrupts.rs

---
+++ src/cpu/interrupts.rs
@@ -0,0 +1,49 @@
+use lazy_static::lazy_static;
+use x86_64::{
+    instructions::segmentation::CS,
+    registers::control::Cr2,
+    structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode},
+};
+
+use super::gdt;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        idt
+    };
+}
+
+pub fn init() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    crate::kprintln!("EXCEPTION: BREAKPOINT\n{stack_frame:#?}");
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) -> ! {
+    crate::kprintln!("EXCEPTION: DOUBLE FAULT ({error_code})\n{stack_frame:#?}");
+    crate::halt_loop()
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    crate::kprintln!(
+        "EXCEPTION: PAGE FAULT at {:?} ({error_code:?})\n{stack_frame:#?}",
+        Cr2::read(),
+    );
+    crate::halt_loop()
+}
